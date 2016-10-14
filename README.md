92f36fdc6160fb5b8a726a8d760cdf494bfae2a3
Liu Nan <153011784@qq.com>
2016-10-14 17:38:17 +0800

    add gif.

--- README.md
+++ README.md
@@ -370,3 +370,73 @@ Glide.with(mContext) //上下文
 
 centerCrop-fitCenter会相互覆盖，后面调用的后覆盖前面调用的效果
 
+
+## Glide入门教程五-加载gif-视频快照-缩略图
+
+### 播放 网络Gif动画
+
+```
+ //加载网络gif
+        String gifUrl = "http://b.hiphotos.baidu.com/zhidao/pic/item/faedab64034f78f066abccc57b310a55b3191c67.jpg";
+        Glide.with(this).load(gifUrl).placeholder(R.mipmap.pictures_no).into(mIv4);
+```
+
+### 本地资源gif
+
+```
+ //加载资源gif
+        Glide.with(this).load(R.mipmap.loading).asGif().placeholder(R.mipmap.pictures_no).into(mIv5);
+
+```
+
+### 加载本地gif文件
+
+
+```
+
+        //加载本地gif文件
+        String gifPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Movies/test.gif";
+        //test.gif  animate.gif Starry_Night.mp4 test.mp4
+
+        File gifFile = new File(gifPath);
+
+        Glide.with(this).load(gifFile).placeholder(R.mipmap.ic_launcher).into(mIv6);
+
+```
+
+### 加载本地视频快照
+
+```
+ //加载本地小视频 快照，只是快照 并不能播放
+        String videoPath =Environment.getExternalStorageDirectory().getAbsolutePath()+"/Movies/test.mp4";;
+
+        File videoFile = new File(videoPath);
+
+        Glide.with(this).load(Uri.fromFile(videoFile)).placeholder(R.mipmap.ic_launcher).into(mIv7);
+```
+
+### 加载缩略图一
+
+
+如果你传递一个0.1f作为参数，Glide会加载原始图片大小的10%的图片。如果原始图片有1000x1000像素，缩略图的分辨率为100x100像素
+
+```
+String urlPath="/storage/emulated/0/MagazineUnlock/magazine-unlock-03-2.3.311-bigpicture_03_20.jpg";
+        //第一种 设置缩略图比例
+        Glide.with(this).load(new File(urlPath)).thumbnail(0.1f).centerCrop().placeholder(R.mipmap.pictures_no).into(mIv8);
+```
+
+### 加载缩略图二 高级缩略图请求：原图与缩略图完全不同
+
+```
+ //先建立一个缩略图对象 先加载缩略图，再加载原图
+        DrawableRequestBuilder thumbnailRequest=  Glide.with(this).load("http://img1.imgtn.bdimg.com/it/u=2615772929,948758168&fm=21&gp=0.jpg");
+
+        Glide.with(this).load(Uri.fromFile(videoFile)).thumbnail(thumbnailRequest).centerCrop().placeholder(R.mipmap.pictures_no).into(mIv9);
+```
+
+
+源码
+<https://github.com/ln0491/GlideDemo>>
+
+
--- app/src/main/java/com/liu/glidedemo/ui/GlideBaseUseActivity.java
+++ app/src/main/java/com/liu/glidedemo/ui/GlideBaseUseActivity.java
@@ -2,9 +2,11 @@ package com.liu.glidedemo.ui;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.bumptech.glide.DrawableRequestBuilder;
 import com.bumptech.glide.Glide;
 import com.liu.glidedemo.R;
 
@@ -19,6 +21,11 @@ public class GlideBaseUseActivity extends AppCompatActivity {
     private ImageView mIv2;
     private ImageView mIv3;
     private ImageView mIv4;
+    private ImageView mIv5;
+    private ImageView mIv6;
+    private ImageView mIv7;
+    private ImageView mIv8;
+    private ImageView mIv9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,11 @@ public class GlideBaseUseActivity extends AppCompatActivity {
         mIv2 = (ImageView) findViewById(R.id.iv2);
         mIv3 = (ImageView) findViewById(R.id.iv3);
         mIv4 = (ImageView) findViewById(R.id.iv4);
+        mIv5 = (ImageView) findViewById(R.id.iv5);
+        mIv6 = (ImageView) findViewById(R.id.iv6);
+        mIv7 = (ImageView) findViewById(R.id.iv7);
+        mIv8 = (ImageView) findViewById(R.id.iv8);
+        mIv9 = (ImageView) findViewById(R.id.iv9);
 
     }
 
@@ -59,9 +71,44 @@ public class GlideBaseUseActivity extends AppCompatActivity {
 
        Glide.with(this).load(uri).into(mIv3);
 
-        //Picasso.with(this).load(file).into(mIv3);
+        //加载网络gif
+        String gifUrl = "http://b.hiphotos.baidu.com/zhidao/pic/item/faedab64034f78f066abccc57b310a55b3191c67.jpg";
+        Glide.with(this).load(gifUrl).placeholder(R.mipmap.pictures_no).into(mIv4);
+
+        //加载资源gif
+        Glide.with(this).load(R.mipmap.loading).asGif().placeholder(R.mipmap.pictures_no).into(mIv5);
+
+
+        //加载本地gif文件
+        String gifPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Movies/test.gif";
+        //test.gif  animate.gif Starry_Night.mp4 test.mp4
+
+        File gifFile = new File(gifPath);
+
+        Glide.with(this).load(gifFile).placeholder(R.mipmap.ic_launcher).into(mIv6);
+
+
+        //加载本地小视频 快照，只是快照 并不能播放
+        String videoPath =Environment.getExternalStorageDirectory().getAbsolutePath()+"/Movies/test.mp4";;
+
+        File videoFile = new File(videoPath);
+
+        Glide.with(this).load(Uri.fromFile(videoFile)).placeholder(R.mipmap.ic_launcher).into(mIv7);
+
+
+        //加载缩略图 /storage/emulated/0/MagazineUnlock/magazine-unlock-03-2.3.311-bigpicture_03_20.jpg
+        ///storage/emulated/0/MagazineUnlock/magazine-unlock-02-2.3.302-bigpicture_02_70.jpg
+
+        String urlPath="/storage/emulated/0/MagazineUnlock/magazine-unlock-03-2.3.311-bigpicture_03_20.jpg";
+        //第一种 设置缩略图比例    先加载缩略图，再加载原图
+        Glide.with(this).load(new File(urlPath)).thumbnail(0.1f).centerCrop().placeholder(R.mipmap.pictures_no).into(mIv8);
+        //第二
+
+        //先建立一个缩略图对象 先加载缩略图，再加载原图  控件移到第一个方便查看
+        DrawableRequestBuilder thumbnailRequest=  Glide.with(this).load("http://img1.imgtn.bdimg.com/it/u=2615772929,948758168&fm=21&gp=0.jpg");
 
 
 
+        Glide.with(this).load(Uri.fromFile(videoFile)).thumbnail(thumbnailRequest).centerCrop().placeholder(R.mipmap.pictures_no).into(mIv9);
     }
 }
